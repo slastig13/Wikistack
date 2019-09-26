c3dc3ef9b620cad0a20a2df99741a91b97acf058
Samantha Lastig <slastig13@gmail.com>
2019-09-26 13:04:02 -0400

    redirect working and different views/routes are displaying

--- views/addPage.js
+++ views/addPage.js
@@ -6,9 +6,21 @@ module.exports = () => layout(html`
   <hr>
   <form method="POST" action="/wiki/">
 
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
+    <!-- <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div> -->
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <div class="col-sm-10">
+        <input id="title" name="title" type="text" class="form-control"/>
+      </div>
+    </div>
 
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
+    <!-- <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div> -->
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <div class="col-sm-10">
+        <input id="title" name="title" type="text" class="form-control"/>
+      </div>
+    </div>
 
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
@@ -17,9 +29,21 @@ module.exports = () => layout(html`
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
+    <!-- <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div> -->
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <div class="col-sm-10">
+        <input id="title" name="title" type="text" class="form-control"/>
+      </div>
+    </div>
 
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+    <!-- <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div> -->
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Page Title</label>
+      <div class="col-sm-10">
+        <input id="title" name="title" type="text" class="form-control"/>
+      </div>
+    </div>
 
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
